6bd7cfcbb595708f778bf3dbb1758815d6216d31
Tilman Marquart <tilman@trieb.work>
2024-11-11 14:32:44 +0100

    feat: sleep fn example

--- app/sleep/route.ts
+++ app/sleep/route.ts
@@ -4,8 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 export async function GET(req: NextRequest) {
-  await sleep(100)
-  revalidatePath("/")
-  console.log("revalidated in timeout")
+  await sleep(100); revalidatePath("/")
+  console.log("revalidated in sleep")
   return NextResponse.json({ revalidated: true })
 }
